5bafda1abc650197e6133f0ed0c39b2182d1c9e5
oskcarsv <oskcarqsv@gmail.com>
2024-09-24 21:45:14 -0600

    finish AcademicManager class

--- SchoolManagementSystemProject/src/main/java/org/example/AcademicManager.java
+++ SchoolManagementSystemProject/src/main/java/org/example/AcademicManager.java
@@ -10,32 +10,68 @@ public class AcademicManager implements AcademicServices {
     private HashMap<Course, ArrayList<Student>> studentPerCourse;
 
     public AcademicManager() {
+        this.students = new ArrayList<>();
+        this.courses = new ArrayList<>();
+        this.studentPerCourse = new HashMap<>();
     }
 
-    public AcademicManager(ArrayList<Student> students, ArrayList<Course> courses, HashMap<Course, ArrayList<Student>> studentPerCourse) {
-        this.students = students;
-        this.courses = courses;
-        this.studentPerCourse = studentPerCourse;
+    private Course getCourseById(int idCourse) {
+        for (Course course : courses) {
+            if (course.getId() == idCourse) {
+                return course;
+            }
+        }
+        return null;
     }
 
+    private Student getStudentById(int idStudent) {
+        for (Student student : students) {
+            if (student.getId() == idStudent) {
+                return student;
+            }
+        }
+        return null;
+    }
 
     @Override
     public void addStudent(Student student) {
-
+        if (!students.contains(student)) {
+            students.add(student);
+        }
     }
 
     @Override
     public void addCourse(Course course) {
-
+        if (!courses.contains(course)) {
+            courses.add(course);
+        }
     }
 
     @Override
     public void enrollStudentInCourse(Student student, int idCourse) throws StudentAlreadyEnrolledException {
-
+        Course course = getCourseById(idCourse);
+        if (course != null) {
+            ArrayList<Student> studentsList = studentPerCourse.getOrDefault(course, new ArrayList<>());
+            if (studentsList.contains(student)) {
+                throw new StudentAlreadyEnrolledException("Student already enrolled in course");
+            } else {
+                studentsList.add(student);
+                studentPerCourse.put(course, studentsList);
+            }
+        }
     }
 
     @Override
     public void unrollStudentInCourse(int idStudent, int idCourse) throws StudentNotEnrolledException {
-
+        Course course = getCourseById(idCourse);
+        if (course != null) {
+            ArrayList<Student> studentsList = studentPerCourse.getOrDefault(course, new ArrayList<>());
+            Student student = getStudentById(idStudent);
+            if (studentsList == null || !studentsList.contains(student)) {
+                throw new StudentNotEnrolledException("Student not enrolled in course");
+            } else {
+                studentsList.remove(student);
+            }
+        }
     }
 }
--- SchoolManagementSystemProject/src/main/java/org/example/Main.java
+++ SchoolManagementSystemProject/src/main/java/org/example/Main.java
@@ -1,7 +1,37 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Create a new student
+        Student student1 = new Student(1, "John", "Doe", new  Date(), "Active");
+        Student student2 = new Student(2, "Jane", "Doe", new  Date(), "Active");
+
+        // Create a new course
+        Course course1 = new Course(1, "Math", "Math course", "5", "1.0");
+        Course course2 = new Course(2, "English", "English course", "5", "1.0");
+
+        // Create a new AcademicManager
+        AcademicManager academicManager = new AcademicManager();
+        academicManager.addStudent(student1);
+        academicManager.addStudent(student2);
+        academicManager.addCourse(course1);
+        academicManager.addCourse(course2);
+
+        // Enroll the student in the course
+        try {
+            academicManager.enrollStudentInCourse(student1, course1.getId());
+            academicManager.enrollStudentInCourse(student2, course2.getId());
+            System.out.println("Student enrolled in course");
+        } catch (StudentAlreadyEnrolledException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Verify if the student is enrolled in the course
+        ArrayList<Student> students = academicManager.getStudentsByCourse(course1);
+
+
     }
 }
